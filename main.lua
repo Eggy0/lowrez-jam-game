daca4cc84a16e63b09f07727c6b17167fff49f67
Eggy0 <37382715+Eggy0@users.noreply.github.com>
2021-08-01 13:54:55 +0200

    anim8

--- main.lua
+++ main.lua
@@ -1,31 +1,31 @@
 require("graphics")
+local anim8 = require ("anim8")
 
-
+local resolutionTest, resolutionTestAnimation
 
 function love.load()
 
 	canvas = love.graphics.newCanvas(canvas_width, canvas_height)
 	canvas:setFilter("linear","nearest")
 	
-	resolution_test = love.graphics.newImage("test.png")
-	animation = newAnimation(resolution_test,64,64,0.3)
-	
+	resolutionTest = love.graphics.newImage("test.png")
+	local resolutionTestGrid = anim8.newGrid(64,64,resolutionTest:getWidth(), resolutionTest:getHeight())
+	local resolutionTestAnimation = anim8.newAnimation(resolutionTestGrid('1-3',1),0.3)
 
 end
 
 function love.update(dt)
-	animation.currentTime = animation.currentTime + dt
-	if animation.currentTime >= animation.duration then
-		animation.currentTime = animation.currentTime - animation.duration
-	end
+
+	resolutionTestAnimation:update(dt)
 
 end
 
 function love.draw()
 	love.graphics.setCanvas(canvas)
 	love.graphics.clear()
-	local spriteNum = math.floor(animation.currentTime / animation.duration * #animation.quads) + 1
-    love.graphics.draw(animation.spriteSheet, animation.quads[spriteNum])
+
+	resolutionTestAnimation:draw(resolutionTest,0,0)
+
 	makeCanvas()
 
 
